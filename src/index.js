2e865f6a5cd3946701463232bed1fd025fc0ecbd
willjw3 <will.j.ward3@gmail.com>
2020-08-30 12:50:23 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from "react-router-dom";
+import "typeface-rubik";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
